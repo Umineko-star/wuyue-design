d3d3a7d9dc221e45f5daed03d495197df8784a05
wuyue <2276224725@qq.com>
2025-03-02 16:22:45 +0800

    style(javascript): 测试eslint
    测试测试

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-	extends: ['@commitlint/config-conventional'],
-	rules: {
-		'type-enum': [
-			2,
-			'always',
-			[
-				'build',
-				'ci',
-				'chore',
-				'docs',
-				'feat',
-				'fix',
-				'perf',
-				'refactor',
-				'revert',
-				'style',
-				'test',
-				'addLog',
-			],
-		],
-	},
-	// extends: ['cz'],
-	// rules: {},
+	// extends: ['@commitlint/config-conventional'],
+	// rules: {
+	// 	'type-enum': [
+	// 		2,
+	// 		'always',
+	// 		[
+	// 			'build',
+	// 			'ci',
+	// 			'chore',
+	// 			'docs',
+	// 			'feat',
+	// 			'fix',
+	// 			'perf',
+	// 			'refactor',
+	// 			'revert',
+	// 			'style',
+	// 			'test',
+	// 			'addLog',
+	// 		],
+	// 	],
+	// },
+	extends: ['cz'],
+	rules: {},
 };
