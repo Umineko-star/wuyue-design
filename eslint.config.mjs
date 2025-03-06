b48e07b497d769c1ae61c7eacb5d07fa0e13a7fe
ext.yangzhaowu3 <ext.yangzhaowu3@jd.com>
2025-03-06 21:24:08 +0800

    feat(javascript): 修改配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,13 +8,14 @@ import { fixupConfigRules } from "@eslint/compat";
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { globals: [globals.browser,globals.node] }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...fixupConfigRules(pluginReactConfig),
   {
     rules: {
-    "@typescript-eslint/no-explicit-any": "off"
+    "@typescript-eslint/no-explicit-any": "off",
+	"@typescript-eslint/no-require-imports": "off"
     }
   }
 ];
